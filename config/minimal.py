a2bab68393d208d78828424b6fbdb15e33c47e15
Robert Lupton the Good <rhl@astro.princeton.edu>
2014-07-09 22:20:12 -0500

    Fiddled with what I call minimal

--- config/minimal.py
+++ config/minimal.py
@@ -1,14 +1,20 @@
-root.detection.thresholdPolarity = "both"
-root.detection.thresholdValue = 6
+if False:
+    root.detection.thresholdPolarity = "both"
+    root.detection.thresholdValue = 6
 
 root.detection.background.isNanSafe = True
 
-root.measurement.algorithms.names.clear()
-for alg in ["shape.sdss", "flux.sinc", "flux.aperture"]:
-    root.measurement.algorithms.names.add(alg)
+for stage in [root.calibrate.initialMeasurement, root.calibrate.measurement, root.measurement]:
+    stage.algorithms.names.clear()
+    for alg in ["shape.sdss", "flux.sinc", "flux.aperture"]:
+        stage.algorithms.names.add(alg)
 
 root.measurement.algorithms["flux.aperture"].radii = [1, 2, 4, 8, 16] # pixels
 
 root.measurement.slots.instFlux = None        # flux.gaussian
 root.measurement.slots.modelFlux = None       # flux.gaussian
 root.measurement.slots.psfFlux = None         # flux.psf
+
+# required by the PSF algorithms we chose
+for alg in ["flux.gaussian", "flux.psf", "flags.pixel"]:
+    root.calibrate.initialMeasurement.algorithms.names.add(alg) 
