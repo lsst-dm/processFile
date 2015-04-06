86a23bb239d4cad3df0ed0b28937dd11300f047f
Robert Lupton the Good <rhl@astro.princeton.edu>
2015-04-06 09:38:24 -0400

    We want our newly detected Footprints

--- bin/processFile.py
+++ bin/processFile.py
@@ -253,6 +253,7 @@ Also includes the PSF model and detection masks.
     config.calibrate.doAstrometry = False
     config.calibrate.doPhotoCal = False
     config.calibrate.detection.reEstimateBackground = False
+    config.detection.returnOriginalFootprints=False
 
     #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
